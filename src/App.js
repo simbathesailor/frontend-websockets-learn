8f2d23cab16ae63b12f1cdfd17fe36078a688938
simbathesailor <anilchaudhary453@gmail.com>
2021-09-12 01:03:10 +0530

    add the frontend base code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+useEffect(() => {
+  
+  const socket = new WebSocket('ws://localhost:8002');
+
+    // Connection opened
+    socket.addEventListener('open', function (event) {
+        socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log("🚀 ~ file: index.html ~ line 22 ~ event", event)
+        console.log('Message from server ', event.data);
+    });
+    
+  return () => {
+   
+  }
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
