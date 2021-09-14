6e43ca104843c27079c972f6a24f240d07a3c34d
simbathesailor <anilchaudhary453@gmail.com>
2021-09-14 16:46:47 +0530

    Update readme with final steps and technologies

--- src/modules/ProductsListing/index.js
+++ src/modules/ProductsListing/index.js
@@ -2,17 +2,22 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import ProductItem from './ProductItem';
 import { useProducts } from './api';
+import PageLoader from '../../common/components/Loader/PageLoader';
 
 import PropTypes from 'prop-types';
 
 export const ProductSectionContainer = styled.div`
 	max-width: 900px;
 	padding: 20px 100px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
 	margin: 0px auto;
 	box-shadow: rgb(0 0 0 / 16%) 0px 1px 4px;
 	background: #fff;
 	margin-top: 100px;
+	min-height: 70vh;
 	@media (max-width: 768px) {
 		padding: 20px 10px;
 		width: 90%;
@@ -55,13 +60,20 @@ function ProductListing(props) {
 
 	return (
 		<ProductSectionContainer>
-			<ProductListContainer>
-				{products.map(product => {
-					return (
-						<ProductItem key={product.id} item={product} getProducts={getProducts} />
-					);
-				})}
-			</ProductListContainer>
+			{stateProducts?.isFetching ? <PageLoader /> : null}
+			{stateProducts?.isFetched ? (
+				<ProductListContainer>
+					{products.map(product => {
+						return (
+							<ProductItem
+								key={product.id}
+								item={product}
+								getProducts={getProducts}
+							/>
+						);
+					})}
+				</ProductListContainer>
+			) : null}
 		</ProductSectionContainer>
 	);
 }
